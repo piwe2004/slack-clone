570283e390953067a2e40400859b18ec28eb1543
origin <piwe2004@gmail.com>
2023-06-13 16:20:54 +0900

    slack 클론코딩

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBVSy4MqAAqRIZ2RnxMo04nQGxG9YTo52Q",
+    authDomain: "slack-clone-cefcc.firebaseapp.com",
+    databaseURL: "https://slack-clone-cefcc-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "slack-clone-cefcc",
+    storageBucket: "slack-clone-cefcc.appspot.com",
+    messagingSenderId: "607341435383",
+    appId: "1:607341435383:web:3d591291d2c348669876f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
