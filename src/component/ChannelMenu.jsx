20f90de5d49d6d5bded3081767102230c0d39d95
origin <piwe2004@gmail.com>
2023-06-16 13:23:08 +0900

    채널작업

---
+++ src/component/ChannelMenu.jsx
@@ -0,0 +1,111 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, TextField } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+import "../firebase";
+import {child, getDatabase, onChildAdded, push, ref, update} from 'firebase/database'
+
+function ChannelMenu() {
+    const [open, setOpen] = useState(false);
+    const [channelName, setChannelName] = useState('');
+    const [channelDetail, setChannelDetail] = useState('');
+    const [channels, setChannels] = useState([]);
+    const [activeChannelId, setActiveChannelId] = useState('');
+    const [firstLoaded, setFirstLoaded] = useState(true)
+    const handleClose = () =>{
+        setOpen(false)
+        setChannelName('')
+        setChannelName('')
+    }
+    const handleOpen = () =>{
+        setOpen(true)
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onChildAdded(ref(getDatabase(), 'channels'), (snapshot)=>{
+            setChannels([...channels, snapshot.val()])
+        })
+        return () => {
+            setChannels([])
+            unsubscribe()
+        }
+    },[]);
+
+    const changeChannel = (channel) => {
+        setActiveChannelId(channel.id)
+    }
+
+    const handleSubmit = useCallback(async () => {
+        const db = getDatabase();
+        const key = push(child(ref(db), 'channels')).key;
+        const newChannel = {
+            id:key,
+            name:channelName,
+            details:channelDetail
+        };
+
+        const updates = {};
+        updates['/channels/'+key] = newChannel;
+
+        try {
+            await update(ref(db), updates)
+            handleClose()
+        } catch (error) {
+            console.error(error)
+        }
+    }, [channelDetail, channelName])
+
+    useEffect(()=>{
+        if(channels.length > 0 && firstLoaded){
+            setActiveChannelId(channels[0].id);
+            setFirstLoaded(false)
+        }
+    },[channels, firstLoaded])
+
+    return (
+        <>
+            {/* TODO 테마반영 */}
+            <List sx={{overflow:'auto', width:240, backgroundColor:'#4c3c4c'}}>
+                <ListItem secondaryAction={
+                    <IconButton sx={{color:'#9a939b'}} onClick={handleOpen}>
+                        <AddIcon />
+                    </IconButton>
+                }>
+                    <ListItemIcon sx={{color:'#9a939b'}} >
+                        <ArrowDropDownIcon />
+                    </ListItemIcon>
+                    <ListItemText primary='채널' sx={{wordBreak:'break-all', color:'#9a939b'}} />
+                </ListItem>
+                <List component='div' disablePadding sx={{pl:3}}>
+                    {
+                        channels.map(channel=>(
+                            <ListItemButton 
+                                key={channel.id} 
+                                onClick={()=>changeChannel(channel)}
+                                selected={channel.id === activeChannelId}
+                            >
+                                <ListItemText primary={`# ${channel.name}`} sx={{wordBreak:'break-all', color:'#918890'}} />
+                            </ListItemButton>
+                        ))
+                    }
+                </List>
+            </List>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>채널 추가</DialogTitle>
+                <DialogContent>
+                    <DialogContentText sx={{marginBottom:'10px'}}>
+                        생성할 채널명과 설명을 입력해주세요.
+                    </DialogContentText>
+                    <TextField autoFocus margin='dense' label='채널명' type='text' fullWidth variant='standard' onChange={(e)=>setChannelName(e.target.value)} />
+                    <TextField autoFocus margin='dense' label='설명' type='text' fullWidth variant='standard' onChange={(e)=>setChannelDetail(e.target.value)} />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>취소</Button>
+                    <Button onClick={handleSubmit}>생성</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    )
+}
+
+export default ChannelMenu
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Box } from '@mui/material';
+import { Box, Drawer, Toolbar } from '@mui/material';
 import Header from '../component/Header';
+import ChannelMenu from '../component/ChannelMenu';
 
 function Main() {
     return (
         // TODO backgroundColor 테마 적용
         <Box sx={{display:'flex', backgroundColor:'white'}}>
             <Header />
+            <Drawer variant='permanent' sx={{width:300}} className='no-scroll'>
+                <Toolbar />
+                <Box sx={{idsplay:'flex', minHeight:'calc( 100vh - 64px )'}} >
+                    <ChannelMenu />
+                </Box>
+            </Drawer>
         </Box>
     )
 }
